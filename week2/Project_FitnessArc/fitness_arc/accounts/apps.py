9495fa01bb95fda48e1b417eab656e13f66465af
Flament Thomas <thomas.flament08@student-cs.fr>
2025-11-12 11:30:05 +0100

    [accounts] add models, signals and apps

--- week2/Project_FitnessArc/fitness_arc/accounts/apps.py
+++ week2/Project_FitnessArc/fitness_arc/accounts/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        # Enregistre les signaux au démarrage de l’app
+        from . import signals  # noqa: F401
+
