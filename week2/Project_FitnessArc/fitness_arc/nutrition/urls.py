0943fce0d6e07790973336c309a1d54df2feacca
Sithisak Remi <remi.sithisak@student-cs.fr>
2025-11-12 20:01:33 +0100

    [nutrition] added etape 7 bis

--- week2/Project_FitnessArc/fitness_arc/nutrition/urls.py
+++ week2/Project_FitnessArc/fitness_arc/nutrition/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Affiche le journal du jour et permet l'ajout
+    path('today/', views.nutrition_today, name='nutrition_today'), 
+    
+    # Endpoint pour traiter l'ajout (peut être intégré dans nutrition_today)
+    # path('add/', views.add_food_log, name='add_food_log'), # Optionnel si tout est dans la vue 'today'
+]
+
