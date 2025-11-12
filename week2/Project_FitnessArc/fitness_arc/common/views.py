df1fe6184229c53d1d26bae50d3539564b5372dc
Darwish Riad <riad.darwish@student-cs.fr>
2025-11-12 20:32:48 +0100

    [main] Page d'accueil

--- week2/Project_FitnessArc/fitness_arc/common/views.py
+++ week2/Project_FitnessArc/fitness_arc/common/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
+
+def index(request):
+    """Page d'accueil du site"""
+    context = {}
+    
+    if request.user.is_authenticated:
+        today = timezone.now().date()
+        context['templates_count'] = request.user.workout_templates.count()
+        context['today_logs_count'] = request.user.food_logs.filter(date=today).count()
+    
+    return render(request, 'common/index.html', context)
