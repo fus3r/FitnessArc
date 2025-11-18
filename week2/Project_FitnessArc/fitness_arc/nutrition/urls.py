8668182dac00ffcdab8c91a748553fe996e49ed6
Darwish Riad <riad.darwish@student-cs.fr>
2025-11-18 23:32:49 +0100

    [nutrition/recipes] Added Recipes feature in Nutrition page. Basic html template

--- week2/Project_FitnessArc/fitness_arc/nutrition/urls.py
+++ week2/Project_FitnessArc/fitness_arc/nutrition/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     
     # NOUVEAU : Route pour supprimer un FoodLog spécifique (identifié par sa PK)
     path('delete/<int:pk>/', views.delete_food_log, name='delete_food_log'),
+    
+    # Recettes
+    path('recipes/', views.recipe_list, name='recipe_list'),
+    path('recipes/<slug:slug>/', views.recipe_detail, name='recipe_detail'),
 ]
 
